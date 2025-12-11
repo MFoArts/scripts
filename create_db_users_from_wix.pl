0f73fbed82ba28d6bc6e16178b56a1151d552b46
Roger Cowles <rojcowles@gmail.com>
2025-12-11 18:00:03 -0500

    Update to test GetOptions()

--- create_db_users_from_wix.pl
+++ create_db_users_from_wix.pl
@@ -4,10 +4,20 @@
 # and build a CSV file in the correct format to import new users to the 
 # MFOA dbase
 
-#!/usr/bin/perl
 use strict;
 use warnings;
 
 use Text::CSV;
 use Getopt::Long;
 use Set::Scalar;
+
+my $dbaseFile = "";
+my @wixFiles = ();
+my $outFile = "";
+
+GetOptions(	'dbaseFile|db=s'	=> \$dbaseFile,
+			'wixFiles|wf=s{,}'		=> \@wixFiles,
+			'outFile|o=s'		=> \$outFile);
+
+#print "Dbase file:\t$dbaseFile\nOut File:\t$outFile\n";
+#for my $wixFile (@wixFiles) { print "Wix File:\t$wixFile\n"; }
